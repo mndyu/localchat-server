d09e2ca6525ca21b4346dd21b7e261c8244e0869
mndyu <uygira@gmail.com>
2020-06-13 22:07:33 +0900

    Fix dev scripts

--- dev-postgres.sh
+++ dev-postgres.sh
@@ -4,8 +4,8 @@
 trap 'docker stop postgres-1' EXIT
 
 # run docker container
-# docker run --rm -d --name postgres-1 -p 127.0.0.1:5432:5432 --env-file postgres/.env -v "$(pwd)"/postgres/pgdata:/var/lib/postgresql/data postgres:12-alpine &&
-docker run --rm -d --name postgres-1 --network host --env-file "$(pwd)"/postgres/.env -v "$(pwd)"/postgres/pgdata:/var/lib/postgresql/data postgres:12-alpine &&
+docker run --rm -d --name postgres-1 -p 127.0.0.1:5432:5432 --env-file postgres/.env -v "$(pwd)"/postgres/pgdata:/var/lib/postgresql/data postgres:12-alpine &&
+# docker run --rm -d --name postgres-1 --network host --env-file "$(pwd)"/postgres/.env -v "$(pwd)"/postgres/pgdata:/var/lib/postgresql/data postgres:12-alpine &&
 
 # run go command
 export $(cat dev-postgres.env | xargs) &&
@@ -13,3 +13,7 @@ export WEB_PUBLIC_DIRECTORY=$DIR/data/public
 export API_SERVER_LOG_DIR=$DIR/data/log/default.log
 export API_SERVER_SEED_FILE=$DIR/data/seed/default.json
 go run ./cmd
+
+
+# stop docker container
+docker stop postgres-1
